f35de065802cb31240c34d1242e6c8a15be289a1
zenglab-regeneration <107344202+zenglab-regeneration@users.noreply.github.com>
2022-08-09 09:37:52 +0800

    Update draw.py

--- inst/code/draw.py
+++ inst/code/draw.py
@@ -4,6 +4,6 @@ import pandas as pd
 dir_root = os.getcwd()
 parameter_settings_f=pd.read_csv(dir_root+'/data/parameter_settings.csv')
 resolution = int(parameter_settings_f['parameter'][0])
-mapping_result = pd.read_csv(dir_root+"/data/" + str(resolution) +"/intermediate_result/comb_position_mapping.csv")
+mapping_csv = pd.read_csv(dir_root+"/data/" + str(resolution) +"/intermediate_result/comb_position_mapping.csv")
 mapping_celltype_pic = dir_root+"/data/" + str(resolution) +"/mapping_result.png"
 util_code.drawPicture(mapping_csv,row_name= "row",col_name="col",colorattribute="celltype",save_file=mapping_celltype_pic,is_save=True,is_show= False,save_type="png")
